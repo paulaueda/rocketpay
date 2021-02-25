fe3e7dbdc77043cc9c3bfd64373d8b6b4cdce0a3
Paula Ueda <paula.ueda@pagar.me>
2021-02-24 21:54:04 -0300

    feat: user creation triggers account creation

--- lib/rocketpay/users/create.ex
+++ lib/rocketpay/users/create.ex
@@ -1,9 +1,42 @@
 defmodule Rocketpay.Users.Create do
-  alias Rocketpay.{Repo, User}
+  # Vai permitir criar user e account em uma mesma trx
+  alias Ecto.Multi
+  alias Rocketpay.{Account, Repo, User}
 
   def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
+    # inicia Multi operação
+    Multi.new()
+    |> Multi.insert(:create_user, User.changeset(params))
+    # o multi run permite ler o resultado da op anterior
+    #  %{nome_da_op_anterior: resultado})
+    |> Multi.run(:create_account, fn repo, %{create_user: user} ->
+      insert_account(repo, user)
+    end)
+    |> Multi.run(:preload_data, fn repo, %{create_user: user} -> preload_data(repo, user) end)
+    |> run_transaction()
+  end
+
+  defp insert_account(repo, user) do
+    user.id
+    |> account_changeset()
+    |> repo.insert()
+  end
+
+  defp account_changeset(user_id) do
+    %{user_id: user_id, balance: "0.00"}
+    |> Account.changeset()
+  end
+
+  defp preload_data(repo, user) do
+    # para devolver o usuario com a conta carregada
+    {:ok, repo.preload(user, :account)}
+  end
+
+  defp run_transaction(multi) do
+    # outra forma de fazer pattern match: case
+    case Repo.transaction(multi) do
+      {:error, _operation, reason, _changes} -> {:error, reason}
+      {:ok, %{preload_data: user}} -> {:ok, user}
+    end
   end
 end
